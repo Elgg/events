0eaa56db17968985fd2ecb14c17f2e9e028c8cbf
cash <cash.costello@gmail.com>
2013-01-21 17:59:43 -0500

    added response logic to ElggEvent

--- classes/ElggEvent.php
+++ classes/ElggEvent.php
@@ -2,9 +2,75 @@
 
 class ElggEvent extends ElggObject {
 
+	/**
+	 * User is attending the event
+	 */
+	const RSVP_YES = 1;
+
+	/**
+	 * User may attend the event
+	 */
+	const RSVP_MAYBE = 0;
+
+	/**
+	 * User is not attending the event
+	 */
+	const RSVP_NO = -1;
+
+	/**
+	 * Annotation name for response
+	 */
+	const RSVP_NAME = "rsvp";
+
 	/** @override */
 	protected function initializeAttributes() {
 		$this->attributes['subtype'] = 'event';
+	}
+
+	public function getAttending($limit) {
+		return $this->getUsersByResponse(ElggEvent::RSVP_YES, $limit);
+	}
+
+	public function getMaybeAttending($limit) {
+		return $this->getUsersByResponse(ElggEvent::RSVP_MAYBE, $limit);
+	}
+
+	public function getNotAttending($limit) {
+		return $this->getUsersByResponse(ElggEvent::RSVP_NO, $limit);
+	}
+
+	/**
+	 * Get users for this event based on how they responded
+	 * 
+	 * @param int $response Response code: RSVP_YES, RSVP_NO, RSVP_MAYBE
+	 * @param int $limit    Maximum number of users to return
+	 * @return array
+	 */
+	protected function getUsersByResponse($response, $limit) {
+		$guid = sanitize_int($this->guid);
+		$nameId = sanitize_int(get_metastring_id(ElggEvent::RSVP_NAME));
+		$valueId = sanitize_int(get_metastring_id($response));
+
+		// @todo is there a way to do this with the API?
+		$db_prefix = elgg_get_config('dbprefix');
+		return elgg_get_entities(array(
+			'type' => 'user',
+			'joins'  => array("JOIN {$db_prefix}annotations a ON e.guid = a.owner_guid"),
+			'wheres' => array(
+				"a.entity_guid = $guid",
+				"a.name_id = $nameId",
+				"a.value_id = $valueId",
+			),
+			'limit' => $limit,
+		));
+	}
+
+	/**
+	 * @param ElggUser $user     The user responding
+	 * @param int      $response Response code: RSVP_YES, RSVP_NO, RSVP_MAYBE
+	 */
+	public function setResponse(ElggUser $user, $response) {
+		$this->annotate(ElggEvent::RSVP_NAME, $response, ACCESS_PUBLIC, $user->guid);
 	}
 
 	/**
